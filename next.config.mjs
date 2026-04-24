b53bf445f6743059d8cc6a9fdd9eeda373addcff
Krzysztof Płonka <info@krzysztofplonka.pl>
2026-04-24 23:41:50 +0200

    naxt.config.mjs edited

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@
  */
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env/server.mjs"));
 
-/** @type {import("next").NextConfig} */
+@type {import("next").NextConfig}
 const config = {
   reactStrictMode: true,
   swcMinify: true,
